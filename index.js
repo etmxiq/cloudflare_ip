383ab79f0bdb008eb2101f7ef267049823289865
etmx <etmx@hotmail.com>
2025-12-05 12:40:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
 export default {
   async fetch(request) {
+    const origin = request.headers.get("Origin");
+
+    const allowed = [
+      "http://mvp.us.kg",   // 允许的网站（你可以加更多）
+      "https://mvp.us.kg",
+    ];
+
+    const allowOrigin = allowed.includes(origin) ? origin : "";
+
+    const corsHeaders = {
+      "Access-Control-Allow-Origin": allowOrigin,
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    };
+
+    if (request.method === "OPTIONS") {
+      return new Response(null, { status: 204, headers: corsHeaders });
+    }
+
     const ip =
       request.headers.get("cf-connecting-ip") ||
       request.headers.get("x-forwarded-for") ||
       "unknown";
 
-    // Cloudflare 提供的访客信息
     const cf = request.cf || {};
 
-    const data = {
-      ip,
-      country: cf.country || null,        // 国家
-      region: cf.region || null,          // 省 / 州
-      city: cf.city || null,              // 城市
-      latitude: cf.latitude || null,      // 纬度
-      longitude: cf.longitude || null,    // 经度
-      postalCode: cf.postalCode || null,  // 邮政编码
-      timezone: cf.timezone || null,      // 时区
-      asn: cf.asn || null,                // 自治系统编号
-      isp: cf.asOrganization || null,     // ISP / 运营商名称
-      continent: cf.continent || null,    // 洲
-      colo: cf.colo || null,              // Cloudflare 入口点机房
-    };
-
-   
-    const headers = {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "http://mvp.us.kg",
-      "Access-Control-Allow-Methods": "GET, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type",
-    };
-
-    // 预检
-    if (request.method === "OPTIONS") {
-      return new Response(null, { status: 200, headers });
-    }
-
-    return new Response(JSON.stringify(data, null, 2), {
-      status: 200,
-      headers,
-    });
+    return new Response(
+      JSON.stringify(
+        {
+          ip,
+          country: cf.country,
+          city: cf.city,
+          colo: cf.colo,
+        },
+        null,
+        2
+      ),
+      {
+        status: 200,
+        headers: corsHeaders,
+      }
+    );
   },
 };
